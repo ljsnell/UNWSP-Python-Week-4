5f923b5463ac8e76266c83fd526141d30bc2b2af
ljsnell <ljsnell7@gmail.com>
2024-08-30 21:32:32 -0500

    Initial unit test.

---
+++ program_2.py
@@ -0,0 +1,14 @@
+# Program #2: Movie Tix
+# Write a program that has the user input various movie names and how many 
+# tickets are desired for each movie.  
+# At the end of the program it prints out the total number of tickets desired by the user.  
+# Use either a "for loop" or "while loop" to accomplish this.
+
+def main():
+    ######################
+    # WRITE YOUR CODE HERE
+    ######################    
+
+
+if __name__ == '__main__':
+    main()
